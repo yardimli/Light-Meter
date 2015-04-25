f3e8d12e46dbf274d5c77fcd3a2401edaaec26ca
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2015-04-25 19:05:52 +0800

    make it work in webview android

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 
+function updateConnectText(newtext) {
+	$("#ConnectButtonText").html(" "+newtext);
+}
+
+function updateDataText(newtext) {
+	$("#luxreading").html(" "+newtext);
+}
+
+
 $(document).ready( function() 
 {
 //	alert(screen.width+" "+screen.height);
+
+	$("#SelectBLEDevice").on('click', function ()
+	{
+		Android.OpenBLEDialog2();
+	});
 });
